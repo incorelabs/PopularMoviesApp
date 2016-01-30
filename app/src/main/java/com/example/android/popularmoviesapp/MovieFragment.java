f8e382661d82c0c49ae37cc104c1b7c3cbfa5e60
Neelabh Pandey <neelabh@guesthouser.com>
2016-01-31 00:51:32 +0530

    Initial Commit, using ViewHolder Pattern.

---
+++ app/src/main/java/com/example/android/popularmoviesapp/MovieFragment.java
@@ -0,0 +1,213 @@
+package com.example.android.popularmoviesapp;
+
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MovieFragment extends Fragment {
+    private MovieListAdapter movieListAdapter;
+
+    public MovieFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Add this line in order for this fragment to handle menu events.
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.movie_list, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_refresh) {
+            FetchMoviesTask moviesTask = new FetchMoviesTask();
+            moviesTask.execute();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ArrayList<MovieCard> moviesList = new ArrayList<MovieCard>();
+
+        movieListAdapter = new MovieListAdapter(getActivity(), moviesList);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_movie);
+        listView.setAdapter(movieListAdapter);
+
+        return rootView;
+    }
+
+    public class FetchMoviesTask extends AsyncTask<Void, Void, ArrayList<MovieCard>> {
+        ArrayList<MovieCard> moviesList = new ArrayList<>();
+
+        private final String LOG_TAG = FetchMoviesTask.class.getSimpleName();
+
+        private ArrayList<MovieCard> getMovieDataFromJson(String moviesJsonStr) throws JSONException {
+            final String TMDB_MOVIE_LIST = "results";
+            final String TMDB_POSTER_PATH = "poster_path";
+            final String TMDB_OVERVIEW = "overview";
+            final String TMDB_RELEASE_DATE = "release_date";
+            final String TMDB_TITLE = "original_title";
+            final String TMDB_USER_RATING = "vote_average";
+
+            JSONObject moviesJson = new JSONObject(moviesJsonStr);
+            JSONArray moviesList = moviesJson.getJSONArray(TMDB_MOVIE_LIST);
+
+            ArrayList<MovieCard> movieCards = new ArrayList<MovieCard>();
+            String title;
+            String releaseDate;
+            float rating;
+
+            for (int i = 0; i < moviesList.length(); i++) {
+                title = moviesList.getJSONObject(i).getString(TMDB_TITLE);
+                releaseDate = moviesList.getJSONObject(i).getString(TMDB_RELEASE_DATE);
+                rating = Float.parseFloat(moviesList.getJSONObject(i).getString(TMDB_USER_RATING));
+                movieCards.add(new MovieCard(title, releaseDate, rating));
+            }
+            return movieCards;
+        }
+
+        @Override
+        protected ArrayList<MovieCard> doInBackground(Void... params) {
+
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String moviesJsonStr = null;
+
+            String sortOrder = "popularity.desc"; // vote_average.desc
+
+            try {
+                // This is the url for v3 of The Movie DB API
+                final String URL_PROTOCOL = "http";
+                final String URL_HOSTNAME = "api.themoviedb.org";
+                final String API_VERSION = "3";
+                final String DISCOVER_PATH = "discover";
+                final String MOVIE_PATH = "movie";
+                final String SORT_PARAM = "sort_by";
+                final String API_KEY = "api_key";
+
+                /* URL -> http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc */
+
+                /* URL -> http://api.themoviedb.org/3/movie/top_rated */
+
+                Uri.Builder tmdbUri = new Uri.Builder();
+                tmdbUri.scheme(URL_PROTOCOL)
+                        .authority(URL_HOSTNAME)
+                        .appendPath(API_VERSION)
+                        .appendPath(DISCOVER_PATH)
+                        .appendPath(MOVIE_PATH)
+                        .appendQueryParameter(SORT_PARAM, sortOrder)
+                        .appendQueryParameter(API_KEY, BuildConfig.THE_MOVIE_DB_API_KEY);
+
+                URL urlFromUri = new URL(tmdbUri.build().toString());
+                Log.v("theMovieDatabase", urlFromUri.toString());
+
+                // Open Connection -> Set Request Type -> Connect
+                urlConnection = (HttpURLConnection) urlFromUri.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the Input Stream from the URL into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                moviesJsonStr = buffer.toString();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+            try {
+                return getMovieDataFromJson(moviesJsonStr);
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, e.getMessage(), e);
+                e.printStackTrace();
+            }
+
+            // This will only happen if there was an error getting or parsing the forecast.
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<MovieCard> movieCards) {
+            if (movieCards != null) {
+                movieListAdapter.clear();
+                movieListAdapter.addAll(movieCards);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/android/popularmoviesapp/MovieListAdapter.java
@@ -0,0 +1,84 @@
+package com.example.android.popularmoviesapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MovieListAdapter extends ArrayAdapter<MovieCard> {
+    private static final String LOG_TAG = MovieListAdapter.class.getSimpleName();
+
+    // View lookup cache
+    private static class ViewHolder {
+        TextView movieTitle;
+        TextView movieRating;
+    }
+
+    /**
+     * This is our own custom constructor (it doesn't mirror a superclass constructor).
+     * The context is used to inflate the layout file, and the List is the data we want
+     * to populate into the lists
+     *
+     * @param context    The current context. Used to inflate the layout file.
+     * @param movieCards A List of AndroidFlavor objects to display in a list
+     */
+    public MovieListAdapter(Context context, ArrayList<MovieCard> movieCards) {
+        // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
+        // the second argument is used when the ArrayAdapter is populating a single TextView.
+        // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
+        // going to use this second argument, so it can be any value. Here, we used 0.
+        super(context, R.layout.list_movie_item, movieCards);
+    }
+
+    /**
+     * Provides a view for an AdapterView (ListView, GridView, etc.)
+     *
+     * @param position    The AdapterView position that is requesting a view
+     * @param convertView The recycled view to populate.
+     *                    (search online for "android view recycling" to learn more)
+     * @param parent      The parent ViewGroup that is used for inflation.
+     * @return The View for the position in the AdapterView.
+     */
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Gets the MovieCard object from the ArrayAdapter at the appropriate position
+        MovieCard movieCard = getItem(position);
+
+        // Check if an existing view is being reused, otherwise inflate the view
+        ViewHolder viewHolder; // view lookup cache stored in tag
+
+        // Adapters recycle views to AdapterViews.
+        // If this is a new View object we're getting, then inflate the layout.
+        // If not, this view already has the layout inflated from a previous call to getView,
+        // and we modify the View widgets as usual.
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.list_movie_item, parent, false);
+            viewHolder.movieTitle = (TextView) convertView.findViewById(R.id.movie_title);
+            viewHolder.movieRating = (TextView) convertView.findViewById(R.id.movie_rating);
+            convertView.setTag(viewHolder);
+
+            /*convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_movie_item, parent, false);*/
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        // Populate the data into the template view using the data object
+        viewHolder.movieTitle.setText(movieCard.movieTitle);
+        viewHolder.movieRating.setText(Float.toString(movieCard.movieRating));
+
+        /*TextView movieTitleView = (TextView) convertView.findViewById(R.id.movie_title);
+        movieTitleView.setText(movieCard.movieTitle);
+
+        TextView movieRatingView = (TextView) convertView.findViewById(R.id.movie_rating);
+        movieRatingView.setText(Float.toString(movieCard.movieRating));*/
+
+        // Return the completed view to render on screen
+        return convertView;
+    }
+}
