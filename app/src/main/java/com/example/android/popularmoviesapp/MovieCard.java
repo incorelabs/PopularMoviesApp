fc514c34dadb979b76ef766d1b38c659418345b4
Neelabh Pandey <incore.labs.np@gmail.com>
2016-02-07 15:04:59 +0530

    Added: New activity for Detail. Fix: The class variables.
    This commit adds a new details activity to show the details as well as
    modified the existing MovieCard class to incorporate more data.

--- app/src/main/java/com/example/android/popularmoviesapp/MovieCard.java
+++ app/src/main/java/com/example/android/popularmoviesapp/MovieCard.java
@@ -3,13 +3,15 @@ package com.example.android.popularmoviesapp;
 public class MovieCard {
     String movieTitle;          // The title of the movie.
     String movieReleaseDate;    // The release date of the movie
-    float movieRating;          // The viewers rating for the movie
     String moviePosterUrl;      // The URL to the movie poster
+    String movieOverview;       // The Plot Synopsis for the movie
+    float movieRating;          // The viewers rating for the movie
 
-    public MovieCard(String movieTitle, String movieReleaseDate, float movieRating, String moviePosterUrl) {
+    public MovieCard(String movieTitle, String movieReleaseDate, String moviePosterUrl, String movieOverview, float movieRating) {
         this.movieTitle = movieTitle;
         this.movieReleaseDate = movieReleaseDate;
-        this.movieRating = movieRating;
         this.moviePosterUrl = moviePosterUrl;
+        this.movieOverview = movieOverview;
+        this.movieRating = movieRating;
     }
 }
