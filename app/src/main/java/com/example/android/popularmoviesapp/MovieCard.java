792e644aaf563ce5d6ea14d9d3157e0f67d752de
Neelabh Pandey <incore.labs.np@gmail.com>
2016-02-08 19:41:31 +0530

    Implemented Parcelable and onSaveInstanceState
    Parcel able has been implemented and a Parcelable object is passed to
    the Detail Activity and basic details displayed there.
    
    onSaveInstanceState() has been implemented to save data during device
    rotation. A few modifications have also been made to onStart to prevent
    unnecessary calls to the API.

--- app/src/main/java/com/example/android/popularmoviesapp/MovieCard.java
+++ app/src/main/java/com/example/android/popularmoviesapp/MovieCard.java
@@ -1,6 +1,9 @@
 package com.example.android.popularmoviesapp;
 
-public class MovieCard {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class MovieCard implements Parcelable {
     String movieTitle;          // The title of the movie.
     String movieReleaseDate;    // The release date of the movie
     String moviePosterUrl;      // The URL to the movie poster
@@ -14,4 +17,38 @@ public class MovieCard {
         this.movieOverview = movieOverview;
         this.movieRating = movieRating;
     }
+
+    protected MovieCard(Parcel in) {
+        movieTitle = in.readString();
+        movieReleaseDate = in.readString();
+        moviePosterUrl = in.readString();
+        movieOverview = in.readString();
+        movieRating = in.readFloat();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(movieTitle);
+        dest.writeString(movieReleaseDate);
+        dest.writeString(moviePosterUrl);
+        dest.writeString(movieOverview);
+        dest.writeFloat(movieRating);
+    }
+
+    public static final Creator<MovieCard> CREATOR = new Creator<MovieCard>() {
+        @Override
+        public MovieCard createFromParcel(Parcel in) {
+            return new MovieCard(in);
+        }
+
+        @Override
+        public MovieCard[] newArray(int size) {
+            return new MovieCard[size];
+        }
+    };
 }
